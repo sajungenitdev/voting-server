57de87093ce92f34500b24be90de5dfbf04130f4
Szamansaju <szamansaju@gmail.com>
2026-05-13 15:24:15 +0600

    some changes done

--- src/app.js
+++ src/app.js
@@ -112,6 +112,7 @@ const allowedOrigins = [
   "http://localhost:5173", // Vite default
   "http://127.0.0.1:3000",
   "http://127.0.0.1:3001",
+  "https://voting-admin-dashboard-ecru.vercel.app",
   process.env.FRONTEND_URL,
   process.env.ADMIN_URL,
 ].filter(Boolean);
